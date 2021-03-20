01b2f3098673655ac8bf83a1847bce72d083eeb5
Elliott DuCharme <ellduc4@gmail.com>
2021-03-19 20:55:31 -0500

    added special chars mode thing to generatePassword()

--- My Stuff/C++ Stuff/PassGenAndGuesser.cpp
+++ My Stuff/C++ Stuff/PassGenAndGuesser.cpp
@@ -9,17 +9,43 @@ using namespace std;
 //http://www.cplusplus.com/forum/general/130902/
 //https://stackoverflow.com/questions/440133/how-do-i-create-a-random-alpha-numeric-string-in-c
 
-//TODO: guesser function can be given the length or not
-
-string generatePassword(int length, bool useNumbers, bool useLowerCase, bool useUpperCase, bool printUsableChars)
+//TODO:
+//guesser function can be given the length or not
+void guessPassword()
 {
-    if (useNumbers == false && useLowerCase == false && useUpperCase == false)
+
+}
+
+int genRandNum(int min, int max)
+{
+    return rand() % (max + 1 - min) + min;
+}
+
+//Generates "random(?)" passwords. User can control how it's generated. If any of the parameters (except for the last one) are -1, random value is used.
+string generatePassword(int length, int useNumbers, int useLowerCase, int useUpperCase, int useSpecialChars, int printUsableChars)
+{
+    if (useNumbers == false && useLowerCase == false && useUpperCase == false && useSpecialChars == false)
     {
-        printf("ERROR. Parameters 2, 3, and/or 4 need to be true :/\n");
+        printf("ERROR. At least one of parameters 2, 3, 4, and/or 5 needs to be true :/\n");
         exit(EXIT_FAILURE);
     }
 
-    vector<char> usableChars; //What chars can be in the password. USe vector for easy resizing
+    if (length == -1)
+        printf("Length specified as -1. Generating random length of %d\n", length = rand());
+
+    if (useNumbers == -1)
+        printf("useNumbers specified as -1. Generating randomly if using or not (0 or 1): %d\n", useNumbers = rand() % 2);
+
+    if (useLowerCase == -1)
+        printf("useLowerCase specified as -1. Generating randomly if using or not (0 or 1): %d\n", useLowerCase = rand() % 2);
+
+    if (useUpperCase == -1)
+        printf("useUpperCase specified as -1. Generating randomly if using or not (0 or 1): %d\n", useUpperCase = rand() % 2);
+
+    if (useSpecialChars == -1)
+        printf("useSpecialChars specified as -1. Generating randomly if using or not (0 or 1): %d\n", useSpecialChars = rand() % 2);
+
+    vector<char> usableChars; //What chars can be in the password. USe vector for ease of use.
     string password;
 
     if (useNumbers)
@@ -40,6 +66,42 @@ string generatePassword(int length, bool useNumbers, bool useLowerCase, bool use
             usableChars.push_back('A' + i);
     }
 
+    if (useSpecialChars)
+    {
+        //I couldn't think of a better way to do this :(
+        usableChars.push_back('`');
+        usableChars.push_back('~');
+        usableChars.push_back('!');
+        usableChars.push_back('@');
+        usableChars.push_back('#');
+        usableChars.push_back('$');
+        usableChars.push_back('%');
+        usableChars.push_back('^');
+        usableChars.push_back('&');
+        usableChars.push_back('*');
+        usableChars.push_back('(');
+        usableChars.push_back(')');
+        usableChars.push_back('-');
+        usableChars.push_back('_');
+        usableChars.push_back('=');
+        usableChars.push_back('+');
+        usableChars.push_back('?');
+        usableChars.push_back('%');
+        usableChars.push_back('[');
+        usableChars.push_back(']');
+        usableChars.push_back('{');
+        usableChars.push_back('}');
+        usableChars.push_back('/');
+        usableChars.push_back('\\');
+        usableChars.push_back('|');
+        usableChars.push_back('<');
+        usableChars.push_back('>');
+        usableChars.push_back(',');
+        usableChars.push_back('.');
+        usableChars.push_back('\'');
+        usableChars.push_back('"');
+    }
+
     if (printUsableChars)
     {
         printf("Usable chars for the password: ");
@@ -55,9 +117,9 @@ string generatePassword(int length, bool useNumbers, bool useLowerCase, bool use
 
 int main()
 {
-    srand(time(0)); //Yes I know rand is bad
-    const int maxLength = 40;
-    int x = rand() % maxLength;
+    srand(time(0)); //Yes, I know rand is bad, especially for something like this.
+    int length = genRandNum(1, 300);
 
-    cout << "\n\nPassword of length " << x << " is: " << generatePassword(x, true, true, true, false) << "\n\n\n";
+    string password = generatePassword(length, true, true, true, true, false);
+    cout << "\n\nPassword of length " << length << " is: " << password << "\n\n\n";
 }
